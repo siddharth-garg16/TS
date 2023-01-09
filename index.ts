400df186318e182dcc636a5ff0516080cb58f443
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-01-09 23:24:51 +0530

    second commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+// let name = "Siddharth";
+// name = 1;
+// console.log(typeof name);
+// console.log(name);
+
+function sum(a:number, b:number):number{
+    return a+b;
+}
+
+console.log(sum(3, 4))
