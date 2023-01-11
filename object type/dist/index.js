78302cd60b7b5ecc7f5c5ca459954101abdb9c19
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-01-11 23:45:30 +0530

    object type

---
+++ object type/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// let fruit:object;
+let fruit1 = {
+    name: "Mango",
+    taste: "Sweet",
+    color: "Yellow",
+    allSeason: true
+};
+console.log(fruit1);
---
+++ object type/src/index.ts
@@ -0,0 +1,44 @@
+// let fruit:object;
+
+// fruit = {
+//     name : "Mango",
+//     taste : "Sweet",
+//     color : "Yello"
+// }
+// fruit.price = 100;
+
+// console.log(fruit);
+// console.log(typeof fruit);
+// console.log(typeof fruit.name);
+
+
+
+// let fruit : {
+//     name:string,
+//     taste:string,
+//     color:string
+// }
+
+// fruit = {
+//     name: "Mango",
+//     taste: "Sweet",
+//     color: "Yello"
+// }
+
+type Fruit = {
+    name: string,
+    taste: string,
+    color: string,
+    allSeason: boolean
+}
+
+let fruit1:Fruit = {
+    name: "Mango",
+    taste: "Sweet",
+    color: "Yellow",
+    allSeason: true
+}
+console.log(fruit1);
+
+
+
