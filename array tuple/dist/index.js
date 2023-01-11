c35090bbe3e84453abce913bbcc034bedb6bb9b1
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-01-12 00:02:35 +0530

    array and tuple type

---
+++ array tuple/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let fruits; //array of strings
+fruits = ["Mango", "Orange", "Banana"];
+console.log(fruits);
+let person = ["Siddharth", "CS", 22]; //tuple
+console.log(person);
---
+++ array tuple/src/index.ts
@@ -0,0 +1,6 @@
+let fruits:string[]; //array of strings
+fruits = ["Mango", "Orange", "Banana"];
+console.log(fruits);
+
+let person:[string, string, number] = ["Siddharth", "CS", 22]; //tuple
+console.log(person);
