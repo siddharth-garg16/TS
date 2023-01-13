f354c6d00e9dff4f1fe4f7540ab0218e161f62a3
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-01-13 22:34:33 +0530

    generics in ts

---
+++ generics/src/index.ts
@@ -0,0 +1,10 @@
+let arrNum = [23, 54, 67, 1, 32, 98, 76, 33]
+let arrStr = ["sr", "tri", "flip", "lol"]
+
+function getRandomElement<T>(arr:T[]):T{
+    const index = Math.floor(Math.random()*arr.length);
+    return arr[index]
+}
+
+console.log(getRandomElement(arrNum));
+console.log(getRandomElement(arrStr));
